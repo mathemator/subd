7bfad43e2acb4b917853e31eee5b1528bd951a10
mathemator <mathemator@yandex.ru>
2025-03-03 00:01:26 +0300

    Revert "Проект: основные задачи"
    This reverts commit 88a5a4eda4ce09cb6bf10b79d45a60f556ae011e.

--- project/scripts/curl/purchase-cycle.sh
+++
@@ -1,6 +0,0 @@
-while true; do
-    echo "$(date) → Отправляем запрос..."
-    curl -X POST "http://host.docker.internal:8080/lapland/make-random-purchase" -H "Content-Type: application/json" -d '{}'
-    echo "$(date) → Запрос отправлен! Ждём 120 секунд..."
-    sleep 120
-done
